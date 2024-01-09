b8862a366f68d16febe98d67c1f0a12909a7f883
Paul LeTourneau <pletourneau62@gmail.com>
2024-01-08 17:29:32 -0800

    style home

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@
 // import "./css/styles.css";
 
 document.addEventListener("DOMContentLoaded", function () {
-  var hamburger = document.getElementById("hamburger");
-  var menu = document.getElementById("menu");
+  // var hamburger = document.getElementById("hamburger");
+  // var menu = document.getElementById("menu");
 
-  hamburger.addEventListener("click", function () {
-    menu.classList.toggle("show");
-  });
+  // hamburger.addEventListener("click", function () {
+  //   menu.classList.toggle("show");
+  // });
+  // var aboutMeDiv = document.getElementById("aboutMe");
+
+  // // Create the first image element
+  // var uxImage = document.createElement("img");
+  // uxImage.src = "../UX.png"; // Update the path if necessary
+  // uxImage.alt = "UX Design";
+
+  // // Create the second image element
+  // var devImage = document.createElement("img");
+  // devImage.src = "../Dev.png"; // Update the path if necessary
+  // devImage.alt = "Developer";
+
+  // aboutMeDiv.appendChild(devImage);
+  // aboutMeDiv.appendChild(uxImage);
+
   const image = document.querySelector(".grid-item.two img");
   image.classList.add("rotating");
 });
